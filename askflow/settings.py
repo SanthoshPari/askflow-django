b70a3da25da75132a5159d2aa6f7be17a3fe4d40
SanthoshPari <santhoshpari33@gmail.com>
2025-12-04 17:20:57 +0530

    Switch Django to PostgreSQL with Docerized db

--- askflow/settings.py
+++ askflow/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -82,11 +82,16 @@ WSGI_APPLICATION = 'askflow.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("DB_NAME", "askflow"),
+        "USER": os.environ.get("DB_USER", "askflow"),
+        "PASSWORD": os.environ.get("DB_PASSWORD", "askflow_password123"),
+        "HOST": os.environ.get("DB_HOST", "db"),
+        "PORT": os.environ.get("DB_PORT", "5432"),
     }
 }
+
 
 
 # Password validation
